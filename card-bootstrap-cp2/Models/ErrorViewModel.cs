03c74652a2efc7e70e11ddcf72a9630404c18fe6
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-05-21 16:27:24 -0300

    Inicializando as configurações do projeto

---
+++ card-bootstrap-cp2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace card_bootstrap_cp2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
